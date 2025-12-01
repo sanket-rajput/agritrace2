f74bba7814a03bc1980e1b17e8c54644da9fa9da
Sanket Rajput <sanketr.co@gmail.com>
2025-12-01 14:45:59 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -12,17 +12,22 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-let app: FirebaseApp;
-let auth: Auth;
-let db: Firestore;
+let app: FirebaseApp | undefined;
+let auth: Auth | undefined;
+let db: Firestore | undefined;
 
-if (!getApps().length) {
-  app = initializeApp(firebaseConfig);
-} else {
-  app = getApp();
+if (firebaseConfig.apiKey && firebaseConfig.projectId) {
+    if (!getApps().length) {
+      app = initializeApp(firebaseConfig);
+    } else {
+      app = getApp();
+    }
+    
+    if (app) {
+        auth = getAuth(app);
+        db = getFirestore(app);
+    }
 }
 
-auth = getAuth(app);
-db = getFirestore(app);
 
 export { app, auth, db };
